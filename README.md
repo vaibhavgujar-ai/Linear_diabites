108f49807f0c0d88e110f6ac8690db0852453b71
vaibhav gujar <am22d401@smail.iitm.ac.in>
2026-01-31 16:50:37 +0530

    this is the commit for app

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 ### diabetes
 ### Software and Tools Requirements
 1.  [Github Account](https://github.com)
-2.  [VS Code IDE] (https://code.visualstudio.com/)
+2.  [VSCodeIDE] (https://code.visualstudio.com/)
+3.  [HerokuAccount](https://heroku.com)
+
+Create new environment 
+'''
+conda create -p venv python==3.7 -y
+'''
---
+++ app.py
@@ -0,0 +1,24 @@
+
+import pickle
+from flask import Flask, request, app, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+# Load the model
+model = pickle.load(open('linear_regression_model_Diabites.pkl','rb'))
+scaler=pickle.load(open('scaler.pkl','rb'))
+@app.route('/')
+def home():
+    return render_template('home.html')       
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output = model.predict(new_data)
+    return jsonify(output[0])
+
+if __name__ == "__main__":
+    app.run(debug=True)
