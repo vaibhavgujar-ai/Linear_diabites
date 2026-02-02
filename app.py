b89c929cbae66c27b801ccbf5a1fbbd62da18556
vaibhav gujar <am22d401@smail.iitm.ac.in>
2026-02-02 18:43:41 +0530

    Flask app working with diabetes prediction UI

--- app.py
+++ app.py
@@ -1,24 +1,32 @@
-
 import pickle
-from flask import Flask, request, app, jsonify, url_for, render_template
+from flask import Flask, request, jsonify, render_template
 import numpy as np
-import pandas as pd
 
 app = Flask(__name__)
-# Load the model
-model = pickle.load(open('linear_regression_model_Diabites.pkl','rb'))
-scaler=pickle.load(open('scaler.pkl','rb'))
+
+model = pickle.load(open("C:\\Users\\hp\\Linear_diabites\\linear_regression_model_Diabites.pkl", "rb"))
+scaler = pickle.load(open("C:\\Users\\hp\\Linear_diabites\\scaler.pkl", "rb"))
+
 @app.route('/')
 def home():
-    return render_template('home.html')       
-@app.route('/predict_api',methods=['POST'])
+    return render_template('home.html')
+
+@app.route('/predict_api', methods=['POST'])
 def predict_api():
     data = request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
-    output = model.predict(new_data)
+    data_array = np.array(list(data.values())).reshape(1,-1)
+    scaled_data = scaler.transform(data_array)
+    output = model.predict(scaled_data)
     return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    output = model.predict(final_input)[0]
+
+    return render_template('home.html',
+    prediction_text=f'The Diabetes Prediction is {output:.2f}')
 
 if __name__ == "__main__":
     app.run(debug=True)
